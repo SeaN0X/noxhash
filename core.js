09cd39447c8a507c65130def067766eca3c5fdb3
SeaN0X <69431500+SeaN0X@users.noreply.github.com>
2022-08-18 11:29:25 -0300

    Update core.js

--- core.js
+++ core.js
@@ -8,7 +8,7 @@ function hash(arg) {
 for (i=0; i<input.length; i+=1) {
     part += input[i]
     if(num){
-        input[i] += ((input[i] * 3) / 3)**2
+        input[i] += ((input[i] * 7) / 15)**3
         num = false
     } else {if(num == false){num = true; input[i] -= input[i]*Math.sqrt(input[i])}}
             outcompiled += (Number(input[i]) * 9) * Math.sqrt(Number(part))
